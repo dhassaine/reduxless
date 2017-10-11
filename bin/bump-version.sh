63da927053b579f87db51c826edd5c6d9a6b1358
Djamel Hassaine <dhassaine@gmail.com>
2017-10-11 10:53:58 +0100

    show debug output

--- bin/bump-version.sh
+++ bin/bump-version.sh
@@ -4,14 +4,14 @@ set -e
 
 # Read the package version into an environment variable
 PACKAGE_VERSION=$(npm info reduxless version)
-
 BRANCH_NAME="release-${PACKAGE_VERSION}+"
 
 # Make a version bump branch
 printf "Create and push a version bump branch $BRANCH_NAME"
-git checkout -b "${BRANCH_NAME}" > /dev/null 2>&1
+git checkout -b "${BRANCH_NAME}"
 npm version patch
-git push origin "${BRANCH_NAME}" > /dev/null 2>&1
+
+git push origin "${BRANCH_NAME}"
 git push --tags
 
 # Open a new GitHub window with a prefilled form
@@ -19,4 +19,7 @@ NEW_PR_URL="https://github.com/dhassaine/reduxless/compare/${BRANCH_NAME}?expand
 printf "Opening a pre-filled \"Create New PR\" form on GitHub, please submit your PR.\n\n"
 printf "When your PR gets accepted, please run npm publish from the master branch.\n\n"
 open $NEW_PR_URL
+
+git checkout master
+git branch -D "${BRANCH_NAME}"
 
