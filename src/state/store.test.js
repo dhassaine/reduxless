00a2f519dac36160635091fa56168de0ff0fedc3
Djamel Hassaine <dhassaine@gmail.com>
2018-01-03 10:51:52 +0000

    withMutations

--- src/state/store.test.js
+++ src/state/store.test.js
@@ -1,67 +1,80 @@
 /* global describe, it, jest */
-import { expect } from 'chai';
-import {createStore} from '../main';
-import {createSelector} from 'reselect';
+import { expect } from "chai";
+import { createStore } from "../main";
+import { createSelector } from "reselect";
 
-describe('Store', () => {
-  it('can be initialised with an initial state', () => {
-    const initialState = {a: 1, b:2};
+describe("Store", () => {
+  it("can be initialised with an initial state", () => {
+    const initialState = { a: 1, b: 2 };
     const store = createStore(initialState);
-    expect(store.get('a')).to.equal(1);
-    expect(store.get('b')).to.equal(2);
+    expect(store.get("a")).to.equal(1);
+    expect(store.get("b")).to.equal(2);
   });
 
-  it('can be subscribed to and calls registered callback on state changes', () => {
+  it("can be subscribed to and calls registered callback on state changes", () => {
     const store = createStore();
     const fn1 = jest.fn();
     const fn2 = jest.fn();
     const unsubscribe1 = store.subscribe(fn1);
     const unsubscribe2 = store.subscribe(fn2);
-    store.set('a', 1);
+    store.set("a", 1);
     expect(fn1.mock.calls).to.have.length(1);
     expect(fn2.mock.calls).to.have.length(1);
     unsubscribe1();
-    store.set('a', 2);
+    store.set("a", 2);
     expect(fn1.mock.calls).to.have.length(1);
     expect(fn2.mock.calls).to.have.length(2);
     unsubscribe2();
-    store.set('a', 3);
+    store.set("a", 3);
     expect(fn1.mock.calls).to.have.length(1);
     expect(fn2.mock.calls).to.have.length(2);
   });
 
-  it('it should return the same reference if the state has not changed', () => {
+  it("allows multiple mount points to be updated by using withMutations", () => {
     const store = createStore();
-    store.set('mount', { a: 1 });
-    expect(store.get('mount')).to.equal(store.get('mount'));
+    const fn1 = jest.fn();
+    const fn2 = jest.fn();
+    store.subscribe(fn1);
+    store.subscribe(fn2);
+    store.withMutations(s => {
+      s.set("a", 1);
+      s.set("b", 2);
+      s.set("c", 3);
+    });
+    expect(fn1.mock.calls).to.have.length(1);
+    expect(fn2.mock.calls).to.have.length(1);
   });
 
-  it('it should still return the same reference if a non relevant part of state has changed', () => {
+  it("it should return the same reference if the state has not changed", () => {
     const store = createStore();
-    store.set('mount', { a: 1 });
-    const ref = store.get('mount');
-    store.set('mount2', { b: 2 });
-    expect(ref).to.equal(store.get('mount'));
+    store.set("mount", { a: 1 });
+    expect(store.get("mount")).to.equal(store.get("mount"));
   });
 
-  it('it should return a different reference if the relevant state has changed', () => {
+  it("it should still return the same reference if a non relevant part of state has changed", () => {
     const store = createStore();
-    store.set('mount', { a: 1 });
-    const ref = store.get('mount');
-    store.set('mount', { b: 2 });
-    expect(ref).to.not.equal(store.get('mount'));
+    store.set("mount", { a: 1 });
+    const ref = store.get("mount");
+    store.set("mount2", { b: 2 });
+    expect(ref).to.equal(store.get("mount"));
   });
 
-  it('should be compatible with reselect library', () => {
+  it("it should return a different reference if the relevant state has changed", () => {
     const store = createStore();
-    store.set('mount', { a: 1, b: 2 });
+    store.set("mount", { a: 1 });
+    const ref = store.get("mount");
+    store.set("mount", { b: 2 });
+    expect(ref).to.not.equal(store.get("mount"));
+  });
 
-    const action = (state, op) => (
-      {
-        a: op(state.a),
-        b: op(state.b)
-      }
-    );
+  it("should be compatible with reselect library", () => {
+    const store = createStore();
+    store.set("mount", { a: 1, b: 2 });
+
+    const action = (state, op) => ({
+      a: op(state.a),
+      b: op(state.b)
+    });
 
     const selector = createSelector(
       state => state.a,
@@ -74,14 +87,14 @@ describe('Store', () => {
 
     const plusOne = x => x + 1;
     const id = x => x;
-    store.set('mount', action(store.get('mount'), plusOne));
+    store.set("mount", action(store.get("mount"), plusOne));
 
-    expect(selector(store.get('mount'))).to.deep.equal({ c: 4, d: 9 });
-    store.set('mount', action(store.get('mount'), id));
-    expect(selector(store.get('mount'))).to.deep.equal({ c: 4, d: 9 });
+    expect(selector(store.get("mount"))).to.deep.equal({ c: 4, d: 9 });
+    store.set("mount", action(store.get("mount"), id));
+    expect(selector(store.get("mount"))).to.deep.equal({ c: 4, d: 9 });
     expect(selector.recomputations()).to.equal(1);
-    store.set('mount', action(store.get('mount'), plusOne));
-    expect(selector(store.get('mount'))).to.deep.equal({ c: 6, d: 12 });
+    store.set("mount", action(store.get("mount"), plusOne));
+    expect(selector(store.get("mount"))).to.deep.equal({ c: 6, d: 12 });
     expect(selector.recomputations()).to.equal(2);
   });
 });
