5c27c4d884ccc15536da1b83928920d56f8ee209
Djamel Hassaine <dhassaine@gmail.com>
2018-01-12 08:46:24 +0000

    demo app use counter exported mountpoint

--- demo/features/counter/actions-selectors.js
+++ demo/features/counter/actions-selectors.js
@@ -1,23 +1,21 @@
-const mountPoint = 'counter';
+export const mountPoint = "counter";
 
 export const selectCounter = store => {
   const counter = store.get(mountPoint);
-  if(counter) {
+  if (counter) {
     return counter.value;
   } else {
     return 0;
   }
 };
 
-export const setCounter = (store, value) => 
-  store.set(mountPoint, {value});
+export const setCounter = (store, value) => store.set(mountPoint, { value });
 
-export const incrementCounter = store => 
+export const incrementCounter = store =>
   setCounter(store, selectCounter(store) + 1);
 
 export const decrementCounter = store =>
   setCounter(store, selectCounter(store) - 1);
 
-export const incrementAsync = store => 
-  Promise.resolve()
-    .then(() => setCounter(store, selectCounter(store) + 1));
+export const incrementAsync = store =>
+  Promise.resolve().then(() => setCounter(store, selectCounter(store) + 1));
--- demo/index.js
+++ demo/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
+import { mountPoint as counterMountPoint } from "./features/counter/actions-selectors";
 import { createStore, Container, enableHistory } from "../src/main";
 import createDocsExample from "./docs-example";
 
 const store = createStore();
 enableHistory(store);
-store.addLocationSync("counter");
+store.addLocationSync(counterMountPoint);
 store.subscribe(() => {
   console.log(store.get("location"));
 });
