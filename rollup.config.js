06d639313ae573f71661d852f7d09835025d533c
Djamel Hassaine <dhassaine@gmail.com>
2018-01-16 11:49:36 +0000

    add json plugin to rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,42 @@
-import babel from 'rollup-plugin-babel';
-import pkg from './package.json';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import babel from "rollup-plugin-babel";
+import pkg from "./package.json";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import json from "rollup-plugin-json";
 
 export default [
-	{
-    input: 'src/main.js',
-    external: ['react', 'react-dom', 'preact', 'inferno-component', 'prop-types'],
-		dest: pkg.browser,
-		format: 'umd',
-		moduleName: 'reduxless',
-		plugins: [
-			resolve(),
-			commonjs(),
-			babel({
-				exclude: ['node_modules/**']
-			})
-		]
-	},
-	{
-    input: 'src/main.js',
-    external: ['react', 'react-dom', 'preact', 'inferno-component'],
-		targets: [
-			{ dest: pkg.main, format: 'cjs' },
-			{ dest: pkg.module, format: 'es' }
-		],
-		plugins: [
-			babel({
-				exclude: ['node_modules/**']
-			})
-		]
-	}
+  {
+    input: "src/main.js",
+    external: [
+      "react",
+      "react-dom",
+      "preact",
+      "inferno-component",
+      "prop-types"
+    ],
+    dest: pkg.browser,
+    format: "umd",
+    moduleName: "reduxless",
+    plugins: [
+      json(),
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: ["node_modules/**"]
+      })
+    ]
+  },
+  {
+    input: "src/main.js",
+    external: ["react", "react-dom", "preact", "inferno-component"],
+    targets: [
+      { dest: pkg.main, format: "cjs" },
+      { dest: pkg.module, format: "es" }
+    ],
+    plugins: [
+      babel({
+        exclude: ["node_modules/**"]
+      })
+    ]
+  }
 ];
