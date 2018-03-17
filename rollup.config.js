efd9cfe140df26a1fbbce260e9975758758eb169
Djamel Hassaine <dhassaine@gmail.com>
2018-03-17 20:33:58 +0000

    provide cjs format, esm and a minified version

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,45 @@
 import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import json from "rollup-plugin-json";
+import minify from "rollup-plugin-babel-minify";
 
-export default {
+const base = {
   input: "src/main.js",
-  external: ["react", "react-dom", "preact", "inferno-component", "prop-types"],
-  output: {
-    file: "dist/reduxless.js",
-    format: "es"
-  },
-  plugins: [
-    json(),
-    resolve(),
-    babel({
-      exclude: ["node_modules/**"],
-      plugins: ["external-helpers"]
-    })
-  ]
+  external: ["react", "react-dom", "preact", "inferno-component", "prop-types"]
 };
+
+const plugins = [
+  json(),
+  resolve(),
+  babel({
+    exclude: ["node_modules/**"],
+    plugins: ["external-helpers"]
+  })
+];
+
+export default [
+  {
+    ...base,
+    output: {
+      file: "dist/reduxless.esm.js",
+      format: "es"
+    },
+    plugins
+  },
+  {
+    ...base,
+    output: {
+      file: "dist/reduxless.js",
+      format: "cjs"
+    },
+    plugins
+  },
+  {
+    ...base,
+    output: {
+      file: "dist/reduxless.min.js",
+      format: "cjs"
+    },
+    plugins: [...plugins, minify({ comments: false, sourceMap: false })]
+  }
+];
