63beb56dd1d4194e82e8b634b37ff08637edcbb6
Klemen Slavič <krof.drakula@gmail.com>
2018-10-22 13:42:25 +0100

    Fix config warning

--- rollup.config.js
+++ rollup.config.js
@@ -22,18 +22,18 @@ export default [
     input: "src/index.ts",
     output: {
       file: "dist/index.esm.js",
-      format: "es"
+      format: "es",
+      sourcemap: true
     },
-    sourcemap: true,
     plugins
   },
   {
     input: "src/index.ts",
     output: {
       file: "dist/index.js",
-      format: "cjs"
+      format: "cjs",
+      sourcemap: true
     },
-    sourcemap: true,
     plugins
   },
   {
