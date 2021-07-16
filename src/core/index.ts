d84f1dbee6681e0d8a95271103b23a2ff5c0173c
Djamel Hassaine <dhassaine@gmail.com>
2021-07-16 11:29:54 +0200

    fix import of getPath

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,6 @@
 import { createRouterEnabledStore } from './router';
+import { getPath } from './router/selectors';
 import createStore from './state/store';
 import selectorMemoizer from './utilities/memoizer';
 
-export { createStore, selectorMemoizer, createRouterEnabledStore };
+export { createStore, selectorMemoizer, createRouterEnabledStore, getPath };
