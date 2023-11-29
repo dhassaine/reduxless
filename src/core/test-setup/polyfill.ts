1b49ffdff890f344a9a36657ceb7fe6f25c16a15
Djamel Hassaine <dhassaine@gmail.com>
2023-11-29 14:33:05 +0100

    upgrade some dev depencies

--- src/core/test-setup/polyfill.ts
+++
@@ -1,8 +0,0 @@
-let id = 0;
-
-global.requestAnimationFrame = (fn) => {
-  setImmediate(() => fn(performance.now()));
-  return ++id;
-};
-
-global.cancelAnimationFrame = (rafId: any) => clearImmediate(rafId);
