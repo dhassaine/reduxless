8cc57634cd90fe8809d36163680ea4d8e18fa240
Djamel Hassaine <dhassaine@gmail.com>
2023-12-01 11:35:48 +0100

    add complimentary test

--- src/core/router/actions.test.ts
+++ src/core/router/actions.test.ts
@@ -5,4 +5,9 @@ describe(generateNewUrl, () => {
     const newUrl = generateNewUrl({ a: undefined }, new Map(), false, '');
     expect(newUrl).toBe('/');
   });
+
+  it('Append the serialized store data to the url if the values are defined', () => {
+    const newUrl = generateNewUrl({ a: 2 }, new Map(), false, '/dashboard');
+    expect(newUrl).toBe('/dashboard?storeData=%7B%22a%22%3A%222%22%7D');
+  });
 });
